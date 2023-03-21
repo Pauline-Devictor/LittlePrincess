2298f835a4deb324c3e53181baa4b74528982648
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2023-03-21 12:22:46 +0100

    Player Movements and speed fix to be smoother

--- Unity/Assets/Planet 3/Scripts/PlayerMovements.cs
+++ Unity/Assets/Planet 3/Scripts/PlayerMovements.cs
@@ -4,42 +4,59 @@ using UnityEngine.Serialization;
 public class PlayerMovements : MonoBehaviour
 { 
     public float moveSpeed;
+    public float maxSpeed;
 
-   public Transform orientation;
+    public Transform orientation;
 
-   private float _horizontalInput;
-   private float _verticalInput;
+    private float _horizontalInput;
+    private float _verticalInput;
 
-   private Vector3 _moveDirection;
-   public Rigidbody rb;
+    private Vector3 _moveDirection;
+    public Rigidbody rb;
 
 
-   private void Start()
-   {
-       rb.freezeRotation = true;
-   }
+    private void Start()
+    {
+        rb.freezeRotation = true;
+    }
 
-   private void Update()
-   {
-       MyInput();
-   }
+    private void Update()
+    {
+        MyInput();
+    }
 
-   private void FixedUpdate()
-   {
-       MovePlayer();
-   }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
-   private void MyInput()
-   {
-       _horizontalInput = Input.GetAxis("Horizontal");
-       _verticalInput = Input.GetAxis("Vertical");
-   }
+    private void MyInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+    }
 
-   private void MovePlayer()
-   {
-       //calculate movement direction
-       _moveDirection = (orientation.forward * _verticalInput) + (orientation.right * _horizontalInput);
-       
-       rb.AddForce(_moveDirection.normalized * moveSpeed, ForceMode.Force);
-   }
+    private void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _moveDirection = (orientation.forward * _verticalInput) + (orientation.right * _horizontalInput);
+
+            if (_verticalInput != 0)
+            {
+                rb.AddForce(_moveDirection.normalized * moveSpeed, ForceMode.Force);
+            }
+
+            rb.velocity = _moveDirection;
+
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+            }
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
 }
