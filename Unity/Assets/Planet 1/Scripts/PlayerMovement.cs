720484ce6479b60b6e7e76af8e748c24a54689dc
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-22 19:46:32 +0100

    la gravité !!!

--- Unity/Assets/Planet 1/Scripts/PlayerMovement.cs
+++ Unity/Assets/Planet 1/Scripts/PlayerMovement.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
 
         var move = transform.right * x + transform.forward * z;
 
         controller.Move(move * (speed * Time.deltaTime));
+        
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
     }
 }
-    
-        
-        
