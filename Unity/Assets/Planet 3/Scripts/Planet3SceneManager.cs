53ca6b45dfc04057f1a04afa605fb4a33a9505a4
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2023-03-29 14:00:49 +0200

    Planet3 Cursor visible depending of the scene

--- Unity/Assets/Planet 3/Scripts/Planet3SceneManager.cs
+++ Unity/Assets/Planet 3/Scripts/Planet3SceneManager.cs
@@ -10,18 +10,24 @@ public class Planet3SceneManager : MonoBehaviour
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         BattleSystem.BattleFinished += SwitchToTown;
         NPCSystem.BattleRequested += SwitchToBattle;
     }
 
     void SwitchToTown()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         battle.SetActive(false);
         town.SetActive(true);
         
     }
     void SwitchToBattle()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         town.SetActive(false);
         battle.SetActive(true);
         BattleStarted?.Invoke();
