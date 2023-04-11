00eb532b743e6e5e7e9dc1a9c9fdb967c0b76465
Ludovic <lulubcancer@hotmail.fr>
2023-04-11 23:37:36 +0200

    J'AI MON PREMIER DIALOGUE

--- Unity/Assets/Planet 2/Scripts/HorizontalMouseLook.cs
+++ Unity/Assets/Planet 2/Scripts/HorizontalMouseLook.cs
@@ -9,6 +9,7 @@ public class HorizontalMouseLook : MonoBehaviour
     
     public float xRotation = 0f;
     
+    
     // Start is called before the first frame update
     void Start()
     {
--- Unity/Assets/Planet 2/Scripts/IntroDetectorP2.cs
+++ Unity/Assets/Planet 2/Scripts/IntroDetectorP2.cs
@@ -11,8 +11,10 @@ public class IntroDetectorP2 : MonoBehaviour
 
     private bool _hasTalked;
     private bool _detectPlayer;
-    
-    
+
+    public GameObject player;
+    public GameObject camera;
+    public GameObject unityChan;
 
     private int _index;
     // Start is called before the first frame update
@@ -27,14 +29,27 @@ public class IntroDetectorP2 : MonoBehaviour
     {
         if (_detectPlayer && !_hasTalked)
         {
-            if ((Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0)) && _index< introductionDialogue.Length)
+            Debug.Log("Player detected");
+            if (_index < introductionDialogue.Length && _index == 0)
             {
+                player.GetComponent<PlayerMovement>().enabled = false;
+                camera.GetComponent<HorizontalMouseLook>().enabled = false;
+                unityChan.GetComponent<PlayerControllerPlanet3>().enabled = false;
+                Debug.Log("------------------------ First line");
                 NextLine();
             }
-
-            if (_index >= introductionDialogue.Length)
+            else if ((Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0)) && _index < introductionDialogue.Length)
             {
+                Debug.Log("------------------------ Next line");
+                NextLine();
+            }
+            else if ((Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0)) && _index == introductionDialogue.Length)
+            {
+                Debug.Log("------------------------ End dialogue");
                 EndDialogue();
+                player.GetComponent<PlayerMovement>().enabled = true;
+                camera.GetComponent<HorizontalMouseLook>().enabled = true;
+                unityChan.GetComponent<PlayerControllerPlanet3>().enabled = true;
                 _hasTalked = true;
             }
             
@@ -43,6 +58,7 @@ public class IntroDetectorP2 : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.name);
         if (other.name.Equals("Player") || other.name.Equals("Unity_Chan_humanoid"))
         {
             _detectPlayer = true;
@@ -58,6 +74,7 @@ public class IntroDetectorP2 : MonoBehaviour
     {
         if (_index < introductionDialogue.Length)
         {
+            dialogueBox.SetActive(true);
             dialogueText.text = introductionDialogue[_index];
             _index++;
         }
