0b62195a11e5acf62517cb3121baa1ebddf79204
Pauline-Devictor <pauline.devictor@etu.unice.fr>
2023-04-11 20:26:33 +0200

    Introduction Animation Over

---
+++ Unity/Assets/AnimationEventRocket.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationEventRocket : MonoBehaviour
+{
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
