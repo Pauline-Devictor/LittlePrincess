2e64c088a7cc79d94123bd3ae17b89f8d337a37a
Ludovic <lulubcancer@hotmail.fr>
2023-04-12 14:08:15 +0200

    Light, 3.2.1, dialogue

---
+++ Unity/Assets/Planet 2/Scripts/GameManagerCourse.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerCourse : MonoBehaviour
+{
+    public PlayerControls playerControls;
+    public AIControls[] aiControls;
+    public LapManager lapTracker;
+    public TricolorLights tricolorLights;
+    public AudioSource audioSource;
+    public AudioClip lowBeep;
+    public AudioClip highBeep;
+    void Awake()
+    {
+        StartGame();
+    }
+    public void StartGame()
+    {
+        FreezePlayers(true);
+        StartCoroutine("Countdown");
+    }
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("3");
+        tricolorLights.SetProgress(1);
+        audioSource.PlayOneShot(lowBeep);
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+        tricolorLights.SetProgress(2);
+        audioSource.PlayOneShot(lowBeep);
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        tricolorLights.SetProgress(3);
+        audioSource.PlayOneShot(lowBeep);
+        yield return new WaitForSeconds(1);
+        Debug.Log("GO");
+        tricolorLights.SetProgress(4);
+        audioSource.PlayOneShot(highBeep);
+        StartRacing();
+        yield return new WaitForSeconds(2f);
+        tricolorLights.SetAllLightsOff();
+    }
+    public void StartRacing()
+    {
+        FreezePlayers(false);
+    }
+    void FreezePlayers(bool freeze)
+    {
+        // log the current state
+        Debug.Log("FreezePlayers: " + freeze);
+        
+        //TODO : freeze players here
+        playerControls.enabled = !freeze;
+        foreach (AIControls ai in aiControls)
+        {
+            ai.enabled = !freeze;
+        }
+    }
+}
